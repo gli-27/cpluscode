869462df57770602f0935a7f23052ea4ce3ba895
Gli <43455032+gli-27@users.noreply.github.com>
2019-07-19 16:07:31 -0400

    Add files via upload

---
+++ chapter2/2.4/2.7/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  2.7
+//
+//  Created by LG on 2019/7/19.
+//  Copyright © 2019 LG. All rights reserved.
+//
+
+#include <iostream>
+
+int main(){
+    std::cout << "Who goes with F\145rgus?\012" << std::endl;
+    std::cout << "3.14e1L: " << 3.14e1L << std::endl;
+    std::cout << "1024f: " << 1024 << std::endl;
+    std::cout << "3.14L: " << 3.14L << std::endl;
+    return 0;
+}
