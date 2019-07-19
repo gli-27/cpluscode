96ee526fd4c44e0400e522f6fd818cb155ba122d
Gli <43455032+gli-27@users.noreply.github.com>
2019-07-19 16:10:01 -0400

    Add files via upload

--- chapter2/2.4/main.cpp
+++ chapter2/2.4/main.cpp
@@ -8,8 +8,8 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(){
+    std::cout << "Just run 2.3 and will print the result of those operations." << std::endl;
+    std::cout << "Since the storage of those variables is binary, then signed variable will set the first bit to represent sign or not. As for signed variable, usually 0 for positive and 1 for negative. Then if there is a signed negative variable output as a unsigned variable, the first bit will be 1 and output the result unexpected."
     return 0;
 }
