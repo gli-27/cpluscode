363b9f687dcbfd08f3b6a1fc4899b67804b5c836
Gli <43455032+gli-27@users.noreply.github.com>
2019-07-27 23:55:03 -0400

    Add files via upload

---
+++ chapter2/2.32/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  2.32
+//
+//  Created by LG on 2019/7/27.
+//  Copyright © 2019 LG. All rights reserved.
+//
+
+#include <iostream>
+
+int main(){
+    //int null = 0, *p = null;
+    
+    std::cout << "int null = 0, *p = null; //Error, since we can not assign an int value to a int pointer type p. If I change this code to a legal one, I prefer to change the int null = 0 to something else, since null is a key word in c++." << std::endl;
+    
+    return 0;
+}
