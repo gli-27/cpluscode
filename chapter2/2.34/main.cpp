59febf2cd47b1c9e73231b71c5711119f927ea53
Gli <43455032+gli-27@users.noreply.github.com>
2019-07-27 23:55:43 -0400

    Add files via upload

---
+++ chapter2/2.34/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2.34
+//
+//  Created by LG on 2019/7/27.
+//  Copyright © 2019 LG. All rights reserved.
+//
+
+#include <iostream>
+
+int main(){
+    std::cout << "Please refer to 2.33." << std::endl;
+    
+    return 0;
+}
