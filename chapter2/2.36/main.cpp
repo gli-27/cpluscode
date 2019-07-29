f99fbeac04ef42a4481250a11d3d8617fe6c1f6d
Gli <43455032+gli-27@users.noreply.github.com>
2019-07-28 22:59:32 -0400

    Add files via upload

---
+++ chapter2/2.36/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  2.36
+//
+//  Created by LG on 2019/7/28.
+//  Copyright © 2019 LG. All rights reserved.
+//
+
+#include <iostream>
+
+int main(){
+    int a = 3, b = 4;
+    decltype(a) c = a;
+    decltype((b)) d = a;
+    ++c;
+    ++d;
+    
+    std::cout << "a's type is: " << typeid(a).name() << " and its value is: " << a << std::endl;
+    
+    std::cout << "b's type is: " << typeid(b).name() << " and its value is: " << b << std::endl;
+    
+    std::cout << "c's type is: " << typeid(c).name() << " and its value is: " << c << std::endl;
+    
+    std::cout << "d's type is: " << typeid(d).name() << " and its value is: " << d << std::endl;
+    
+    return 0;
+}
