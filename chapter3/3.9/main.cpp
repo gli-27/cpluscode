945822de837cf734c5ac0bf77d59d237feccf9c6
Gli <43455032+gli-27@users.noreply.github.com>
2019-08-18 18:26:45 -0400

    Add files via upload

---
+++ chapter3/3.9/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  3.9
+//
+//  Created by LG on 2019/8/18.
+//  Copyright © 2019 LG. All rights reserved.
+//
+
+#include <iostream>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(){
+    string s;
+    cout << s[0] << endl;
+    cout << "This will output result of \n" << endl;
+    return 0;
+}
